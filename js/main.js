0db4ec2e870c1ac5a03a823b9867110dde451359
unknown <b24b24testing.ru>
2019-06-10 05:46:21 +0300

    Заданы массив с комментариями и массив с именами комментаторов

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ var getRandomInt = function (min, max) {
   var randomInt = Math.round(Math.random() * (max - min));
   return randomInt;
 };
+
+var COMMENT_PHRASES = [
+  'Всё отлично!',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+];
+
+var NAMES = [
+  'Артём',
+  'Сергей',
+  'Иван',
+  'Михаил',
+  'Руслан',
+];
