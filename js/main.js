b5286cda9b727d0d130d3c0698890cfc7f75260d
unknown <b24b24testing.ru>
2019-06-14 08:58:19 +0300

    Полностью переработан код задания: упрощены функции, структура стала более ясной.

--- js/main.js
+++ js/main.js
@@ -21,79 +21,58 @@ var getRandomInt = function (min, max) {
   return randomInt;
 };
 
-var getPhotosDescriptor = function (numberOfPictures, likesMinNumber, likesMaxNumber) {
-  var photosDescription = [];
+var getPictureObjects = function (numberOfPictures) {
+  var pictureObjects = [];
 
   for (var i = 0; i < numberOfPictures; i++) {
-    photosDescription[i] =
+    pictureObjects[i] =
             {
               url: 'photos/' + (i + 1) + '.jpg',
-              likes: getRandomInt(likesMinNumber, likesMaxNumber),
+              likes: getRandomInt(LIKES_MIN_NUMBER, LIKES_MAX_NUMBER),
               comments: [],
             };
     var maxNumberOfComments = getRandomInt(0, 2); // 0 - 1 комментарий, 1 - 2 комментария, 2 - 3 комментария
     for (var j = 0; j <= maxNumberOfComments; j++) {
-      photosDescription[i].comments[j] = {
+      pictureObjects[i].comments[j] = {
         avatar: 'img/avatar-' + (i + 1) + '.svg',
         message: COMMENT_PHRASES [getRandomInt(0, COMMENT_PHRASES.length - 1)],
         name: NAMES [getRandomInt(0, NAMES.length - 1)],
       };
     }
   }
-  return photosDescription;
+  return pictureObjects;
 };
 
-var createUsersPictures = function (numberOfPictures, likesMinNumber, likesMaxNumber) {
-  var photosDescription = getPhotosDescriptor(numberOfPictures, likesMinNumber, likesMaxNumber);
+
+var getUserPictureDomElement = function (pictureObject) {
   var template = document.querySelector('#picture');
   var picture = template.content.querySelector('.picture').cloneNode(true);
-  var pictureObjectsArray = [];
-  for (var i = 0; i < photosDescription.length; i++) {
-    var templateContent = {
-      img: picture.querySelector('.picture__img').cloneNode(true),
-      likes: picture.querySelector('.picture__likes').cloneNode(true),
-      comments: [],
-    };
-    for (var j = 0; j < photosDescription[i].comments.length; j++) {
-      templateContent.comments[j] = picture.querySelector('.picture__likes').cloneNode(true);
-      templateContent.comments[j].textContent = photosDescription[i].comments[j].message;
-    }
-    templateContent.img.src = photosDescription[i].url;
-    templateContent.likes.textContent = photosDescription[i].likes;
-    pictureObjectsArray[i] = {
-      img: templateContent.img,
-      likes: templateContent.likes,
-      comments: [],
-    };
-    for (j = 0; j < photosDescription[i].comments.length; j++) {
-      pictureObjectsArray[i].comments[j] = templateContent.comments[j];
-    }
+  picture.querySelector('.picture__img').src = pictureObject.url;
+  picture.querySelector('.picture__likes').textContent = pictureObject.likes;
+  picture.querySelector('.picture__comments').textContent = pictureObject.comments.length;
+  return picture;
+};
+
+var getUserPictureDomElements = function (pictureObjects) {
+  var pictures = [];
+  for (var i = 0; i < pictureObjects.length; i++) {
+    pictures[i] = getUserPictureDomElement(pictureObjects[i]);
   }
-  return pictureObjectsArray;
+  return pictures;
 };
 
-var getPictureDocumentFragment = function (pictureObjectsArray) {
+var insertUserPictureDomElement = function (pictureDomElement) {
+  var documentFragmentVar = new DocumentFragment();
 
-  var pictureDocumentFragment = new DocumentFragment();
+  documentFragmentVar.appendChild(pictureDomElement);
+  var parent = document.querySelector('.pictures');
+  parent.appendChild(documentFragmentVar);
+};
 
-  for (var i = 0; i < pictureObjectsArray.length; i++) {
-    var template = document.querySelector('#picture').cloneNode(true);
-    var reference = template.content.querySelector('.picture').cloneNode();
-    var paragraph = template.content.querySelector('.picture__info').cloneNode(true);
-    var spanComments = paragraph.querySelector('.picture__comments').cloneNode();
-    var spanLikes = paragraph.querySelector('.picture__likes').cloneNode();
-    spanComments.textContent = pictureObjectsArray[i].comments.length;
-    spanLikes.textContent = pictureObjectsArray[i].likes;
-    reference.appendChild(pictureObjectsArray[i].img);
-    reference.appendChild(paragraph);
-    pictureDocumentFragment.appendChild(reference);
+var insertUserPictureDomElements = function (pictureDomElements) {
+  for (var i = 0; i < pictureDomElements.length; i++) {
+    insertUserPictureDomElement(pictureDomElements[i]);
   }
-  return pictureDocumentFragment;
 };
 
-var insertDocumentFragment = function (pictureDocumentFragment, parentClass) {
-  var section = document.querySelector('.' + parentClass);
-  section.appendChild(pictureDocumentFragment);
-};
-
-insertDocumentFragment(getPictureDocumentFragment(createUsersPictures(25, LIKES_MIN_NUMBER, LIKES_MAX_NUMBER)), 'pictures');
+insertUserPictureDomElements(getUserPictureDomElements(getPictureObjects(25)));
