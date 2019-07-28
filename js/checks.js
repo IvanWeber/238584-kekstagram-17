c52ac989cad5bedfc28ca763a76602b8e814cb33
unknown <b24b24testing.ru>
2019-07-28 06:05:04 +0300

    Вынесены в модуль checks.js массивы с проверками для валидации поля хэштегов и поля текстового описания

---
+++ js/checks.js
@@ -0,0 +1,106 @@
+'use strict';
+
+(function () {
+  window.checks = {
+
+    TextareaChecks: [{
+      checker: function (value) {
+        return value.length < window.Validation.MESSAGE_MAX_LENGTH;
+      },
+      message: 'Длина сообщения должна быть меньше 140 символов',
+    }
+    ],
+
+    HashtagsChecks: [
+      {
+        checker: function (value) {
+          return value.length < window.Validation.MESSAGE_MAX_LENGTH;
+        },
+        message: 'Длина сообщения должна быть меньше 140 символов',
+      },
+      {
+        checker: function (value) {
+          var valueArray = value.split('');
+          return valueArray[0] === '#';
+        },
+        message: 'Поле должно начинаться с символа #'
+      },
+      {
+        checker: function (value) {
+          var valueArray = value.split('');
+          for (var i = 0; i < valueArray.length; i++) {
+            if (valueArray[i] === ' ' && valueArray[i + 1] !== '#') {
+              return false;
+            }
+          }
+          return true;
+        },
+        message: 'Каждый новый хэш-тег после пробела должен начинаться с символа #'
+      },
+      {
+        checker: function (value) {
+          var valueArray = value.split('');
+          for (var i = 0; i < valueArray.length; i++) {
+            if (valueArray[i] === '#' && valueArray[i + 1] === ' ' || valueArray[i] === '#' && valueArray[i + 1] === undefined) {
+              return false;
+            }
+          }
+          return true;
+        },
+        message: 'Хэш-тег не может состоять только из одной решётки'
+      },
+      {
+        checker: function (value) {
+          var valueArray = value.split('');
+          for (var i = 0; i < valueArray.length; i++) {
+            if (valueArray[i - 1] !== undefined && valueArray[i - 1] !== ' ' && valueArray[i] === '#') {
+              return false;
+            }
+          }
+          return true;
+        },
+        message: 'Хэш-теги разделяются пробелами'
+      },
+      {
+        checker: function (value) {
+          var valueArray = value.split('');
+          for (var i = 0; i < valueArray.length; i++) {
+            valueArray[i] = valueArray[i].toLowerCase();
+          }
+          var newValue = valueArray.join('');
+          valueArray = newValue.split(' ');
+          for (var j = 0; j < valueArray.length; j++) {
+            for (var k = 0; k < valueArray.length; k++) {
+              if (valueArray[j] === valueArray[k] && j !== k) {
+                return false;
+              }
+            }
+          }
+          return true;
+        },
+        message: 'Один и тот же хэш-тег не может быть использован дважды. Теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом'
+      },
+      {
+        checker: function (value) {
+          var valueArray = value.split(' ');
+          var isInvalid = valueArray.length > window.Validation.MAX_NUMBER_OF_HASHTAGS;
+          return isInvalid ? false : true;
+        },
+        message: 'Нельзя указать больше пяти хэш-тегов'
+      },
+      {
+        checker: function (value) {
+          var hashtagsArray = value.split(' ');
+          for (var i = 0; i < hashtagsArray.length; i++) {
+            if (hashtagsArray[i].split('').length > window.Validation.MAX_CHARS_OF_HASHTAG) {
+              return false;
+            }
+          }
+          return true;
+        },
+        message: 'Максимальная длина одного хэш-тега 20 символов, включая решётку'
+      }
+    ]
+
+  };
+})();
--- js/form.js
+++ js/form.js
@@ -66,8 +66,8 @@
       initiateChangeFilterOnChangeFilterRadioButton();
       closeEditFormOnKeydownEscAndOnClickCloseButton();
       initiateChangeImgSizeOnClickPlusMinusButtons();
-      initiateCheckOnChangeElementOfForm('.text__description', TextareaChecks);
-      initiateCheckOnChangeElementOfForm('.text__hashtags', HashtagsChecks);
+      initiateCheckOnChangeElementOfForm('.text__description', window.checks.TextareaChecks);
+      initiateCheckOnChangeElementOfForm('.text__hashtags', window.checks.HashtagsChecks);
     };
     input.addEventListener('change', pictureInputChangeHandler);
   };
@@ -120,105 +120,6 @@
     };
     commentary.addEventListener('keydown', commentaryElementKeydownEscHandler);
   };
-
-  var TextareaChecks = [{
-    checker: function (value) {
-      return value.length < window.Validation.MESSAGE_MAX_LENGTH;
-    },
-    message: 'Длина сообщения должна быть меньше 140 символов',
-  }
-  ];
-
-  var HashtagsChecks = [
-    {
-      checker: function (value) {
-        return value.length < window.Validation.MESSAGE_MAX_LENGTH;
-      },
-      message: 'Длина сообщения должна быть меньше 140 символов',
-    },
-    {
-      checker: function (value) {
-        var valueArray = value.split('');
-        return valueArray[0] === '#';
-      },
-      message: 'Поле должно начинаться с символа #'
-    },
-    {
-      checker: function (value) {
-        var valueArray = value.split('');
-        for (var i = 0; i < valueArray.length; i++) {
-          if (valueArray[i] === ' ' && valueArray[i + 1] !== '#') {
-            return false;
-          }
-        }
-        return true;
-      },
-      message: 'Каждый новый хэш-тег после пробела должен начинаться с символа #'
-    },
-    {
-      checker: function (value) {
-        var valueArray = value.split('');
-        for (var i = 0; i < valueArray.length; i++) {
-          if (valueArray[i] === '#' && valueArray[i + 1] === ' ' || valueArray[i] === '#' && valueArray[i + 1] === undefined) {
-            return false;
-          }
-        }
-        return true;
-      },
-      message: 'Хэш-тег не может состоять только из одной решётки'
-    },
-    {
-      checker: function (value) {
-        var valueArray = value.split('');
-        for (var i = 0; i < valueArray.length; i++) {
-          if (valueArray[i - 1] !== undefined && valueArray[i - 1] !== ' ' && valueArray[i] === '#') {
-            return false;
-          }
-        }
-        return true;
-      },
-      message: 'Хэш-теги разделяются пробелами'
-    },
-    {
-      checker: function (value) {
-        var valueArray = value.split('');
-        for (var i = 0; i < valueArray.length; i++) {
-          valueArray[i] = valueArray[i].toLowerCase();
-        }
-        var newValue = valueArray.join('');
-        valueArray = newValue.split(' ');
-        for (var j = 0; j < valueArray.length; j++) {
-          for (var k = 0; k < valueArray.length; k++) {
-            if (valueArray[j] === valueArray[k] && j !== k) {
-              return false;
-            }
-          }
-        }
-        return true;
-      },
-      message: 'Один и тот же хэш-тег не может быть использован дважды. Теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом'
-    },
-    {
-      checker: function (value) {
-        var valueArray = value.split(' ');
-        var isInvalid = valueArray.length > window.Validation.MAX_NUMBER_OF_HASHTAGS;
-        return isInvalid ? false : true;
-      },
-      message: 'Нельзя указать больше пяти хэш-тегов'
-    },
-    {
-      checker: function (value) {
-        var hashtagsArray = value.split(' ');
-        for (var i = 0; i < hashtagsArray.length; i++) {
-          if (hashtagsArray[i].split('').length > window.Validation.MAX_CHARS_OF_HASHTAG) {
-            return false;
-          }
-        }
-        return true;
-      },
-      message: 'Максимальная длина одного хэш-тега 20 символов, включая решётку'
-    }
-  ];
 
   var initiateCheckOnChangeElementOfForm = function (elementSelector, checks) {
     var element = document.querySelector(elementSelector);
